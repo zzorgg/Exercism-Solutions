14e7963fa88ce9bd49d1d3a61a4d6a2bdc6de473
saifk16 <saif.khan16@outlook.com>
2025-04-25 16:15:00 +0530

    calculator

--- java/calculator-conundrum/src/main/java/CalculatorConundrum.java
+++ java/calculator-conundrum/src/main/java/CalculatorConundrum.java
@@ -1,5 +1,29 @@
 class CalculatorConundrum {
     public String calculate(int operand1, int operand2, String operation) {
-        throw new UnsupportedOperationException("Please implement the CalculatorConundrum.calculate() method");
+        String s = "";
+        if (operation == null) {
+            throw new IllegalArgumentException("Operation cannot be null");
+        } else if(operation == "") {
+            throw new IllegalArgumentException("Operation cannot be empty");    
+        } else {
+            switch (operation) {
+                case "+":
+                s = operand1 + " + " + operand2 + " = " + (operand1 + operand2);
+                break;
+                case "*":
+                s = operand1 + " * " + operand2 + " = " + (operand1 * operand2);
+                break;
+                case "/":
+                try {
+                    s = operand1 + " / " + operand2 + " = " + (operand1 / operand2);
+                } catch (ArithmeticException e) {
+                    throw new IllegalOperationException("Division by zero is not allowed", e);
+                }
+                break;
+                default:
+                throw new IllegalOperationException("Operation '" + operation + "' does not exist");
+            }
+        }
+        return s;
     }
 }
