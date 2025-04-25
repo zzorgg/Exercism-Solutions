038878fff3cc928da8e6ee4acb51687508420423
saifk16 <saif.khan16@outlook.com>
2025-04-25 16:40:48 +0530

    two fer refactored

--- java/two-fer/src/main/java/Twofer.java
+++ java/two-fer/src/main/java/Twofer.java
@@ -1,11 +1,5 @@
 public class Twofer {
     public String twofer(String name) {
-        String s = "";
-        if(name == "" || name == null) {
-            s = "One for you, one for me.";
-        } else {
-            s = "One for " + name + ", one for me.";
-        }
-        return s;
+        return (name == "" || name == null) ? "One for you, one for me." : "One for " + name + ", one for me.";
     }
 }
